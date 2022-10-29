f1d4ed753d510d04a34cefe4a53f534b5ac1f4d1
MarcosPauloJS <marcospaulooj@gmail.com>
2022-10-29 16:36:48 -0300

    add styled system to project and config theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { ThemeProvider } from 'styled-components';
 import './App.css';
+import { theme } from './theme';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    primary: '#D8464C',
+    secondary: '#F2B84B',
+    light: 'F2EAD0',
+    dark: '#222940',
+    lightDark: '#34406A' 
+  },
+  fontSize: {
+    small: '10px',
+    medium: '14px',
+    big: '16px',
+    bigest: '48px'
+  }
+}
